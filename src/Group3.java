c4294e17140d7245044dac1240c228a5e69c477f
Harry <lv000013@morris.umn.edu>
2023-10-24 21:18:40 -0500

    Improve sort method

--- src/Group3.java
+++ src/Group3.java
@@ -1,4 +1,6 @@
 package src;
+
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
@@ -9,99 +11,91 @@ import java.util.HashMap;
 
 public class Group3 {
 
-	public static void main(String[] args) throws InterruptedException, FileNotFoundException {
+    public static void main(String[] args) throws InterruptedException, FileNotFoundException {
 
-		if (args.length < 2) {
-			System.out.println(
-					"Please run with two command line arguments: input and output file names");
-			System.exit(0);
-		}
+        if (args.length < 2) {
+            System.out.println(
+                    "Please run with two command line arguments: input and output file names");
+            System.exit(0);
+        }
 
-		String inputFileName = args[0];
-		String outFileName = args[1];
+        String inputFileName = args[0];
+        String outFileName = args[1];
 
-		String[] data = readData(inputFileName);
-		
-		String[] toSort = data.clone();
-		
-		sort(toSort);
+        String[] data = readData(inputFileName);
 
-		toSort = data.clone();
-		
-		Thread.sleep(10);
-		
-		long start = System.currentTimeMillis();
-		sort(toSort);
-		long end = System.currentTimeMillis();
-		
-		System.out.println(end - start);
+        String[] toSort = data.clone();
 
-		writeOutResult(toSort, outFileName);
+        sort(toSort);
 
-	}
+        toSort = data.clone();
 
-	private static String[] readData(String inputFileName) throws FileNotFoundException {
-		ArrayList<String> input = new ArrayList<>();
-		Scanner in = new Scanner(new File(inputFileName));
-		while (in.hasNext()) {
-			input.add(in.next());
-		}
-		in.close();
-		return input.toArray(new String[0]);
-	}
+        Thread.sleep(10);
 
-	// Method to find the sum of prime factors of a number 'n'
-	private static int sumPrimeFactors(int n, HashMap<Integer, Integer> temp) {
-		// Check if the result is already computed
-		if (temp.containsKey(n)) {
-			return temp.get(n);
-		}
-		int sum = 0;
-		int originalN = n;
-		// Loop up to the square root of 'n'
-		for (int i = 2; i <= Math.sqrt(n); i++) {
-			// Check if 'i' is a factor
-			if (n % i == 0) {
-				sum += i;
-				// Eliminate all occurrences of 'i'
-				while (n % i == 0) {
-					n /= i;
-				}
-			}
-		}
-		// If 'n' is greater than 1, it is a prime number.
-		if (n > 1) {
-			sum += n;
-		}
-		// Store the computed result for future use
-		temp.put(originalN, sum);
-		return sum;
-	}
+        long start = System.currentTimeMillis();
+        sort(toSort);
+        long end = System.currentTimeMillis();
 
-	// Method to sort the array based on the sum of prime factors
-	private static void sort(String[] toSort) {
-		// Temporary HashMap to store pre-computed sums of prime factors
-		HashMap<Integer, Integer> temp = new HashMap<>();
-		int[] parsedNumbers = new int[toSort.length];
-		for (int i = 0; i < toSort.length; i++) {
-			parsedNumbers[i] = Integer.parseInt(toSort[i]);
-		}
-		// Sort using custom comparator
-		Arrays.sort(toSort, (a, b) -> {
-			int numA = Integer.parseInt(a);
-			int numB = Integer.parseInt(b);
-			int compare = sumPrimeFactors(numA, temp) - sumPrimeFactors(numB, temp);
-			if (compare == 0) {
-				return numB - numA;
-			}
-			return compare;
-		});
-	}
-	private static void writeOutResult(String[] sorted, String outputFilename) throws FileNotFoundException {
-		PrintWriter out = new PrintWriter(outputFilename);
-		for (String s : sorted) {
-			out.println(s);
-		}
-		out.close();
-	}
+        System.out.println(end - start);
+
+        writeOutResult(toSort, outFileName);
+
+    }
+
+    private static String[] readData(String inputFileName) throws FileNotFoundException {
+        ArrayList<String> input = new ArrayList<>();
+        Scanner in = new Scanner(new File(inputFileName));
+        while (in.hasNext()) {
+            input.add(in.next());
+        }
+        in.close();
+        return input.toArray(new String[0]);
+    }
+
+    // Method to find the sum of prime factors of a number 'n'
+    private static void sumPrimeFactors(int n, HashMap<Integer, Integer> temp) {
+        int sum = 0;
+        int originalN = n;
+        // Loop up to the square root of 'n'
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                sum += i;
+                while (n % i == 0) {
+                    n /= i;
+                }
+            }
+        }
+        if (n > 1) {
+            sum += n;
+        }
+        temp.put(originalN, sum);
+    }
+
+    // Method to sort the array based on the sum of prime factors
+    private static void sort(String[] toSort) {
+        HashMap<Integer, Integer> temp = new HashMap<>();
+        // Pre-compute the sum of prime factors for all numbers
+        for (String s : toSort) {
+            int num = Integer.parseInt(s);
+            sumPrimeFactors(num, temp);
+        }
+        // Sort using the pre-computed values
+        Arrays.sort(toSort, (a, b) -> {
+            int numA = Integer.parseInt(a);
+            int numB = Integer.parseInt(b);
+            int compare = temp.get(numA) - temp.get(numB);
+            if (compare == 0) {
+                return numB - numA;
+            }
+            return compare;
+        });
+    }
+
+    private static void writeOutResult(String[] sorted, String outputFilename) throws FileNotFoundException {
+        PrintWriter out = new PrintWriter(outputFilename);
+        for (String s : sorted) {
+            out.println(s);
+        }
+        out.close();
+    }
 }
